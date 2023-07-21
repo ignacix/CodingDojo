3f9556758cc8a3058e712b6a70d8b31ed005056e
ignacix <ignanj8mail.com>
2023-07-21 17:56:16 -0300

    Se agrega el ejercicio Tu Propio Programa Java

---
+++ Java/JavaFundamentals/TuPropioProgramaJava/ProgramaJava.java
@@ -0,0 +1,7 @@
+public class ProgramaJava {
+    public static void main(String[] args){
+        System.out.println("Mi nombre es Ignacio Jerez");
+        System.out.println("Tengo 24 años de edad.");
+        System.out.println("Mi ciudad es Maquinista Savio, Buenos Aires");
+    }
+}
