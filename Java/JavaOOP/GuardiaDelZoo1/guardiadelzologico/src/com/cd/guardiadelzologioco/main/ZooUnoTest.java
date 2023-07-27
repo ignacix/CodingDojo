7b8acee24096e164b140676448965d09fec5b978
ignacix <ignanj8mail.com>
2023-07-27 17:40:46 -0300

    Se agrega el ejercicio Guardia del Zoológico 1

---
+++ Java/JavaOOP/GuardiaDelZoo1/guardiadelzologico/src/com/cd/guardiadelzologioco/main/ZooUnoTest.java
@@ -0,0 +1,23 @@
+package com.cd.guardiadelzologioco.main;
+import domain.Gorilla; 
+
+public class ZooUnoTest{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		Gorilla mono1 = new Gorilla();
+		System.out.println(mono1.throwSomething());
+		System.out.println(mono1.throwSomething());
+		System.out.println(mono1.throwSomething());
+		System.out.println(mono1.displayEnergy());
+		System.out.println(mono1.eatBananas());
+		System.out.println(mono1.eatBananas());
+		System.out.println(mono1.displayEnergy());
+		System.out.println(mono1.climb());
+		System.out.println(mono1.displayEnergy());
+		
+
+	}
+
+}
