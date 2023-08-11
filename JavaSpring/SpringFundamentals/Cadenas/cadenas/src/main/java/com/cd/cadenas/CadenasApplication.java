479e11e1a6111588b5ead4b9e71d9a9edcc0d88c
ignacix <ignanj8mail.com>
2023-08-11 00:45:30 -0300

    Se agrega el ejercicio Cadenas

---
+++ JavaSpring/SpringFundamentals/Cadenas/cadenas/src/main/java/com/cd/cadenas/CadenasApplication.java
@@ -0,0 +1,25 @@
+package com.cd.cadenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CadenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadenasApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String random() {
+		return "Spring Boot is great! So easy to just respond with strings";
+	}
+}
---
+++ JavaSpring/SpringFundamentals/Cadenas/cadenas/src/test/java/com/cd/cadenas/CadenasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.cadenas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadenasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
